6937f73cab704fecf8e7f23838a238b729b77123
bbtgnn <bbt.gnn@gmail.com>
2019-02-10 23:22:25 +0100

    sketch updates

--- sketch.js
+++ sketch.js
@@ -1,18 +1,22 @@
 var canvas;
+var t=0;
+var rad;
 
 function setup() {
   canvas = createCanvas(window.innerWidth, window.innerHeight);
+  rad = width/10
 }
 
 function draw() {
   background(51);
   fill(255);
-  ellipse(width/2,height/2,100,100);
+  ellipse(width/2+rad*cos(t),height/2+rad*sin(t),100,100);
+  t += 0.05;
 }
 
 window.onresize = function() {
   var w = window.innerWidth;
-  var h = window.innerHeight;  
+  var h = window.innerHeight;
   canvas.size(w,h);
   width = w;
   height = h;
