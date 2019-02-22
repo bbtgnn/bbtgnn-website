d8fe75f1c45b1e58911531e26aa8c2a8ab6cb110
bbtgnn <bbt.gnn@gmail.com>
2019-02-22 02:31:34 +0100

    Sketch update

--- sketch.js
+++ sketch.js
@@ -1,23 +1,32 @@
 var canvas;
-var t=0;
-var rad;
+
+
+let xoff = 0.0;
+let xoff2 = 0.5;
+
+
+function getDivSize() {
+  var clientHeight = document.getElementById('sketch-holder').clientHeight;
+  var clientWidth = document.getElementById('sketch-holder').clientWidth;
+  return [clientWidth, clientHeight];
+}
 
 function setup() {
-  canvas = createCanvas(window.innerWidth, window.innerHeight);
-  rad = width/10
+  canvas = createCanvas(getDivSize()[0], getDivSize()[1]);
+  canvas.parent('sketch-holder');
 }
 
 function draw() {
   background(51);
-  fill(255);
-  ellipse(width/2+rad*cos(t),height/2+rad*sin(t),100,100);
-  t += 0.05;
+
+  xoff = xoff + 0.01;
+  xoff2 = xoff2 - 0.007;
+  let n = noise(xoff) * width;
+  line(n, 0, n, height);
+  let n2 = noise(xoff2) * width;
+  line(n2, 0, n2, height);
 }
 
-window.onresize = function() {
-  var w = window.innerWidth;
-  var h = window.innerHeight;
-  canvas.size(w,h);
-  width = w;
-  height = h;
-};
+function windowResized() {
+  resizeCanvas(getDivSize()[0], getDivSize()[1]);
+}
