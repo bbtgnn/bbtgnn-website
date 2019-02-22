ed0d9dd117e69170f0e90be001fbc0d73cdddcbb
bbtgnn <bbt.gnn@gmail.com>
2019-02-22 14:39:41 +0100

    Updated sketch

--- sketch.js
+++ sketch.js
@@ -1,37 +1,45 @@
-var canvas;
-
+// --- VARIABLES --- //
 var mrg = 30;
 
+var noise_1 = 0.0;
+var noise_2 = 0.5;
 
-let xoff = 0.0;
-let xoff2 = 0.5;
 
 
-function getDivSize() {
-  var clientHeight = document.getElementById('sketch-canvas').clientHeight;
-  var clientWidth = document.getElementById('sketch-canvas').clientWidth;
-  return [clientWidth, clientHeight];
-}
-
+// --- P5 --- //
 function setup() {
-  canvas = createCanvas(getDivSize()[0], getDivSize()[1]);
+  // Getting div size for canvas size
+  var [canvas_w, canvas_h] = getDivSize('sketch-canvas');
+  var canvas = createCanvas(canvas_w, canvas_h);
+  // Setting canvas div
   canvas.parent('sketch-canvas');
-  strokeWeight(mrg);
-  strokeCap(SQUARE);
-  stroke(255, 0, 0);
+  // Setting rect styles
+  noStroke();
+  // blendMode(EXCLUSION);
 }
 
 function draw() {
   background(250);
+  // Updating perlin noise
+  noise_1 = noise_1 + 0.01;
+  noise_2 = noise_2 - 0.007;
+  let x1 = noise(noise_1) * width;
+  fill(255, 0, 0, 100);
+  rect(x1/2, mrg, x1, height-2*mrg);
+  let x2 = noise(noise_2) * width;
+  fill(255, 255, 0, 100);
+  rect(x2/2, mrg, x2, height-2*mrg);
+}
 
-  xoff = xoff + 0.01;
-  xoff2 = xoff2 - 0.007;
-  let n = noise(xoff) * width;
-  line(n, mrg, n, height-mrg);
-  let n2 = noise(xoff2) * width;
-  line(n2, mrg, n2, height-mrg);
+
+
+// --- PLAIN JAVASCRIPT --- //
+function getDivSize(div_name) {
+  var div_w = document.getElementById(div_name).clientWidth;
+  var div_h = document.getElementById(div_name).clientHeight;
+  return [div_w, div_h];
 }
 
 function windowResized() {
-  resizeCanvas(getDivSize()[0], getDivSize()[1]);
+  resizeCanvas(getDivSize('sketch-canvas')[0], getDivSize('sketch-canvas')[1]);
 }
