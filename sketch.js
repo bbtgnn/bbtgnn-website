321a94880ec3de5ea45775c08e57a4d660c090a0
bbtgnn <bbt.gnn@gmail.com>
2019-02-22 03:21:59 +0100

    Time to sleep

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 var canvas;
 
+var mrg = 30;
+
 
 let xoff = 0.0;
 let xoff2 = 0.5;
@@ -14,17 +16,20 @@ function getDivSize() {
 function setup() {
   canvas = createCanvas(getDivSize()[0], getDivSize()[1]);
   canvas.parent('sketch-canvas');
+  strokeWeight(mrg);
+  strokeCap(SQUARE);
+  stroke(255, 0, 0);
 }
 
 function draw() {
-  background(51);
+  background(250);
 
   xoff = xoff + 0.01;
   xoff2 = xoff2 - 0.007;
   let n = noise(xoff) * width;
-  line(n, 0, n, height);
+  line(n, mrg, n, height-mrg);
   let n2 = noise(xoff2) * width;
-  line(n2, 0, n2, height);
+  line(n2, mrg, n2, height-mrg);
 }
 
 function windowResized() {
